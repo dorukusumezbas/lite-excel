6549eb9b87101ce404d3da7e7f15d55dabf848c0
dorukusumezbas <d.usumezbas@gmail.com>
2019-05-08 18:43:36 +0300

    as

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import authNebim
 import orderStores
 import orderWholesale
 import products
+import schedule
+import time
 
-sessionID = authNebim.main()
-client = authGoogle.main()
-orderStores.main(sessionID, client)
-orderWholesale.main(sessionID, client)
-products.main(sessionID, client)
+def main():
+    sessionID = authNebim.main()
+    client = authGoogle.main()
+    orderStores.main(sessionID, client)
+    orderWholesale.main(sessionID, client)
+    products.main(sessionID, client)
+
+main()
+schedule.every().hour.do(main)
+print("scheduled")
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
